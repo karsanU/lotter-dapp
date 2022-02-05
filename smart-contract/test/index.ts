dba8bbd50dda40b97f99fd8aa1a650a07491b86e
Karsan Uthayakumar <Karsan.uthayakumar@gmail.com>
2022-02-05 17:30:19 -0500

    basic setup

---
+++ smart-contract/test/index.ts
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Lottery", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy("Hello, world!");
+    await lottery.deployed();
+
+    expect(await lottery.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await lottery.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await lottery.greet()).to.equal("Hola, mundo!");
+  });
+});
