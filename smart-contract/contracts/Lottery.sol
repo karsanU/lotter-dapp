dddba4a69e5a554853731638421c435dcad7b2f5
Karsan Uthayakumar <Karsan.uthayakumar@gmail.com>
2022-02-06 17:24:03 -0500

    Lottery contract updated

--- smart-contract/contracts/Lottery.sol
+++ smart-contract/contracts/Lottery.sol
@@ -1,22 +1,80 @@
 //SPDX-License-Identifier: Unlicense
+
 pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "hardhat/console.sol";
 
 contract Lottery {
-    string private greeting;
+    address payable public owner;
+    IERC20 public bbt;
+    address[2] public mangers;
+    address payable[] public players;
+    uint256 public ticketPrice;
+    uint256 public currPool;
+    uint256 public lastDarwTime;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor(IERC20 token) {
+        console.log("Deploying Lotterty. Owner: ", msg.sender);
+        owner = payable(msg.sender);
+        bbt = token;
+        ticketPrice = 25;
+        lastDarwTime = block.timestamp;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function setTicketPrice(uint256 _ticketPrice) public ownerAcess {
+        ticketPrice = _ticketPrice;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function enter(uint256 _totalTikets) public payable {
+        require(_totalTikets > 0, "Must by atleast one tiket.");
+        /// transfer the tokens to lottery contract
+        bbt.transferFrom(msg.sender, address(this), ticketPrice * _totalTikets);
+        /// add tokens to the current pool
+        currPool = ticketPrice * _totalTikets;
+        /// enter the user _totalTikets number of times
+        for (uint256 i = 0; i < _totalTikets; i++) {
+            players.push(payable(msg.sender));
+        }
+    }
+
+    function draw() public fiveMinsPassed managerAcess {
+        uint256 index = random() % players.length;
+        address payable _winner = (players[index]);
+        players = new address payable[](0);
+        _winner.transfer(address(this).balance);
+    }
+
+    modifier ownerAcess() {
+        require(msg.sender == owner, "Sender is not owner.");
+        _;
+    }
+
+    modifier managerAcess() {
+        require(
+            msg.sender == owner ||
+                mangers[0] == msg.sender ||
+                mangers[1] == msg.sender,
+            "Sender is not manager"
+        );
+        _;
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
+    }
+
+    modifier fiveMinsPassed() {
+        require(
+            lastDarwTime + 5 minutes < block.timestamp,
+            "5 minutes has to pass."
+        );
+        _;
     }
 }
--- smart-contract/test/index.ts
+++ smart-contract/test/index.ts
@@ -1,7 +1,20 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lottery", function () {
+describe("BBT Token Contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
+
+describe("Lottery Contract", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Lottery = await ethers.getContractFactory("Lottery");
     const lottery = await Lottery.deploy("Hello, world!");
@@ -17,3 +30,5 @@ describe("Lottery", function () {
     expect(await lottery.greet()).to.equal("Hola, mundo!");
   });
 });
+
+
