bfced5ba1528a59bbeada4d13d4b033dc75933e8
Karsan Uthayakumar <Karsan.uthayakumar@gmail.com>
2022-02-19 16:45:36 -0500

    add testnet deploy changes

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -25,8 +25,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.2",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
