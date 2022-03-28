5791b53b9ca64897271ad91b5703dbe62cf6566a
Karsan Uthayakumar <Karsan.uthayakumar@gmail.com>
2022-03-27 21:25:51 -0400

    updating contract addresses

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const LotteryAddress = "0x66bD0F0711875E8475aa916210Af569Fd88fF1Cb";
-export const BBTAddress = "0xbEAC2BcFd87Fc6E53a05756b2E63dF90B9AA6Bed";
+export const LotteryAddress = "0xbAc25BbfDe44687ebB5E73c896DDf32789bF5f1C";
+export const BBTAddress = "0x7cB1CC3383870a5535e009D0dBf028783EA51de7";
